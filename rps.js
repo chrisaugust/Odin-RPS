e414ccb121b68fb0ca191ab76e1eb29a77465a94
Chris Danielson <christopher.a.danielson@protonmail.com>
2022-09-17 11:35:25 -0600

    Fix scoring bug

--- rps.js
+++ rps.js
@@ -4,7 +4,7 @@ let numComputerWins = 0;
 
 function game() {
 
-  for (let i = 0; i < 5; i++) {                                      
+  while (numPlayerWins < 5 &&  numComputerWins < 5) {                                      
     let roundData = playRound();
     let playerSelection = roundData['player'];
     let computerSelection = roundData['computer'];
@@ -12,13 +12,12 @@ function game() {
 
 
     alert(`You chose ${playerSelection}, computer chose ${computerSelection}`);
-    alert(determineRoundWinner(playerSelection, computerSelection));
-  } 
+    alert(resultsMessage);
 
   alert(`player: ${numPlayerWins}, computer: ${numComputerWins}`);
-                                                                         
+  }                                                                         
   if (numPlayerWins > numComputerWins) {
-    console.log("You won!");
+    alert("You won!");
   } else if (numPlayerWins < numComputerWins) {
     alert("You lost :(");
   } else {
@@ -31,7 +30,6 @@ function playRound() {
   let computerSelection = computerPlay();
   let roundResults = determineRoundWinner(playerSelection, computerSelection);
 
-
   return { 'player': playerSelection, 
            'computer': computerSelection, 
            'result': roundResults, 
@@ -43,21 +41,23 @@ function computerPlay() {
 }
 
 function determineRoundWinner(playerSelection, computerSelection) {
+  let message;
 
   if (playerSelection === 'Rock') {
+
     switch (computerSelection) {
       case 'Rock': 
-        return "It's a tie!";
+        message = "It's a tie!";
         break;
 
       case 'Paper': 
-        numComputerWins++; 
-        return "Paper beats Rock, you lose.";
+        numComputerWins += 1; 
+        message = "Paper beats Rock, you lose.";
         break;
 
       case 'Scissors': 
-        numPlayerWins++;
-        return "Rock beats Scissors, you win!";
+        numPlayerWins += 1;
+        message = "Rock beats Scissors, you win!";
         break;
     }
 
@@ -65,37 +65,39 @@ function determineRoundWinner(playerSelection, computerSelection) {
 
     switch (computerSelection) {                                     
       case 'Rock': 
-        numPlayerWins++;
-        return "Paper beats Rock, you win!";
+        numPlayerWins += 1;
+        message = "Paper beats Rock, you win!";
         break;
                                                                      
       case 'Paper': 
-        return "It's a tie!";
+        message = "It's a tie!";
         break;
                                                                     
       case 'Scissors': 
-        numComputerWins++;
-        return "Scissors beat Paper, you lose.";
+        numComputerWins += 1;
+        message = "Scissors beat Paper, you lose.";
         break;
     }
   } else if (playerSelection === 'Scissors') {
 
     switch (computerSelection) {
       case 'Rock': 
-        numComputerWins++;
-        return "Rock beats Scissors, you lose.";
+        numComputerWins += 1;
+        message = "Rock beats Scissors, you lose.";
         break;
                                                                      
       case 'Paper': 
-        numPlayerWins++;
-        return "Scissors beat Paper, you win!";
+        numPlayerWins += 1;
+        message = "Scissors beat Paper, you win!";
         break;
                                                                      
       case 'Scissors': 
-        return "It's a tie!";
+        message = "It's a tie!";
         break;
     }
   }
+
+  return message;
 }
 
 game();
